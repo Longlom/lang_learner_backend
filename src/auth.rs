1ad481c526138783dbc041656b36d4d515822d1c
longlom <long.lom@yandex.ru>
2024-11-14 08:55:17 +0300

    LLB: add tokens generation

--- src/auth.rs
+++ src/auth.rs
@@ -2,8 +2,11 @@ use std::convert::Infallible;
 
 use crate::db::Database;
 use base64ct::{Base64, Encoding};
+use chrono::{DateTime, Duration, Local};
+use jwt::{token::Signed, Header, SignWithKey, Token};
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
+use hmac::{Hmac, Mac};
 use warp::{http::StatusCode, reply::Reply, Filter};
 
 const ALLOWED_LANGUAGE: [&str; 2] = ["VN", "CH"];
@@ -61,19 +64,143 @@ pub struct LoginPayload {
 
 #[derive(sqlx::FromRow, Clone, Deserialize, Serialize)]
 pub struct UserData {
+    id: i32,
     username: String,
     language: Language,
 }
 
-#[derive(sqlx::Type, Clone, Deserialize, Serialize, Debug)]
+#[derive(sqlx::Type, Clone, Copy, Deserialize, Serialize, Debug)]
 #[sqlx(type_name = "language")]
-enum Language { VN, CH }
+enum Language {
+    VN,
+    CH,
+}
+type JwtToken = Token<Header, TokenData, Signed>;
 
-pub async fn login(login_payload: LoginPayload, db: Database) -> Result<impl warp::Reply, Infallible> {
+type Time = DateTime<Local>;
+
+#[derive(Deserialize, Serialize)]
+pub struct TokenData {
+    id: i32,
+    language: Language,
+    username: String,
+    expires_in: Time,
+}
+
+enum JwtType {
+    ACCESS,
+    REFRESH,
+}
+
+impl TokenData {
+    fn new(user_data: &UserData, token_type: JwtType) -> Self {
+        match token_type {
+            JwtType::ACCESS => {
+                let expires_in = Local::now() + Duration::hours(24);
+                return Self {
+                    id: user_data.id,
+                    language: user_data.language,
+                    username: user_data.username.clone(),
+                    expires_in,
+                };
+            }
+
+            JwtType::REFRESH => {
+                let expires_in = Local::now() + Duration::hours(168);
+                return Self {
+                    id: user_data.id,
+                    language: user_data.language,
+                    username: user_data.username.clone(),
+                    expires_in,
+                };
+            },
+        }
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+
+pub struct TokensData {
+    access_token: String,
+    refresh_token: String,
+}
+
+
+impl TokensData {
+    pub fn new(access_token: JwtToken, refresh_token: JwtToken) -> Self {
+        Self {
+            access_token: access_token.as_str().to_string(),
+            refresh_token: refresh_token.as_str().to_string(),
+        }
+    }
+}
+
+fn create_token(user: UserData) -> Result<TokensData, StatusCode> {
+    let key_str = match std::env::var("KEY") {
+        Ok(key_str) => key_str,
+        Err(_err) => {
+            log::error!("KEY must be set");
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
+    };
+
+    let key: Hmac<Sha256> = match Hmac::new_from_slice(key_str.as_bytes()) {
+        Ok(key) => key,
+        Err(err) => {
+            log::error!("{}", err);
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
+    };
+
+    let header = Header {
+        algorithm: jwt::AlgorithmType::Hs256,
+        type_: Some(jwt::header::HeaderType::JsonWebToken),
+        ..Default::default()
+    };
+
+    let access_token = match Token::new(header, TokenData::new(&user, JwtType::ACCESS)).sign_with_key(&key) {
+        Ok(token) => token,
+        Err(err) => {
+            eprintln!("{}", err);
+            panic!();
+        }
+    };
+
+    let key: Hmac<Sha256> = match Hmac::new_from_slice(key_str.as_bytes()) {
+        Ok(key) => key,
+        Err(err) => {
+            log::error!("{}", err);
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
+    };
+
+    let header = Header {
+        algorithm: jwt::AlgorithmType::Hs256,
+        type_: Some(jwt::header::HeaderType::JsonWebToken),
+        ..Default::default()
+    };
+
+    let refresh_token: JwtToken = match Token::new(header, TokenData::new(&user, JwtType::REFRESH)).sign_with_key(&key) {
+        Ok(token) => token,
+        Err(err) => {
+            eprintln!("{}", err);
+            panic!();
+        }
+    };
+
+    let response = TokensData::new(access_token, refresh_token);
+
+    Ok(response)
+}
+
+pub async fn login(
+    login_payload: LoginPayload,
+    db: Database,
+) -> Result<impl warp::Reply, Infallible> {
     log::debug!("Login : {:?}", login_payload);
 
-    let mut login;
-    let mut password;
+    let login;
+    let password;
 
     match (login_payload.login, login_payload.password) {
         (Some(l), Some(p)) => {
@@ -88,32 +215,36 @@ pub async fn login(login_payload: LoginPayload, db: Database) -> Result<impl war
 
     let hash = Sha256::digest(&password);
     let password_hash = Base64::encode_string(&hash);
-    let user = match sqlx::query_as::<_, UserData>("SELECT username, language FROM users WHERE (password = $1 AND login = $2)")
-        .bind(&password_hash)
-        .bind(&login)
-        .fetch_all(&db.conn_pool).await {
-            Ok(res) => {
-                if res.len() > 1 {
-                    log::error!("Found multiple users with username - {}", login);
-                    return Ok(StatusCode::BAD_REQUEST.into_response());
-                }
+    let user = match sqlx::query_as::<_, UserData>(
+        "SELECT id, username, language FROM users WHERE (password = $1 AND login = $2)",
+    )
+    .bind(&password_hash)
+    .bind(&login)
+    .fetch_all(&db.conn_pool)
+    .await
+    {
+        Ok(res) => {
+            if res.len() > 1 {
+                log::error!("Found multiple users with username - {}", login);
+                return Ok(StatusCode::BAD_REQUEST.into_response());
+            }
 
-                if res.len() == 0 {
-                    return Ok(StatusCode::UNAUTHORIZED.into_response());
-                }
+            if res.len() == 0 {
+                return Ok(StatusCode::UNAUTHORIZED.into_response());
+            }
 
             res[0].clone()
-            },
-            Err(err) => {
-                log::error!("Error happened while finding user - {}", err);
-                return Ok(StatusCode::INTERNAL_SERVER_ERROR.into_response());
-            }
-        };
+        }
+        Err(err) => {
+            log::error!("Error happened while finding user - {}", err);
+            return Ok(StatusCode::INTERNAL_SERVER_ERROR.into_response());
+        }
+    };
+
+    let tokens = create_token(user).unwrap();
 
 
+    let json = warp::reply::json(&tokens);
 
-        let json = warp::reply::json(&user);
-
-        return Ok(warp::reply::with_status(json, StatusCode::OK).into_response());
-
+    return Ok(warp::reply::with_status(json, StatusCode::OK).into_response());
 }
